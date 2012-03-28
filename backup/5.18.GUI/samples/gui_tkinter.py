a4466df554d981a929b7078f819a8003e7641647
Avinash Ramu <trackavinash@gmail.com>
2012-03-28 13:39:50 -0500

    cleanup

--- backup/5.18.GUI/samples/gui_tkinter.py
+++
@@ -1,8 +0,0 @@
-from sys import exit
-from Tkinter import *
-import subprocess
-root = Tk() 
-Button(root, text='Click to Exit !', command=subprocess.call('./a.out', shell=True)).pack() 
-# Simple command
-
-root.mainloop()
